a7440c99d7f058b7fbe7f6b515b198ae4a33ae6a
Willem Melching <willem.melching@gmail.com>
2022-03-11 12:31:26 +0100

    also prepare wide

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -74,16 +74,17 @@ ModelOutput* model_eval_frame(ModelState* s, VisionBuf* buf, VisionBuf* wbuf,
   // if getInputBuf is not NULL, net_input_buf will be
   auto net_input_buf = s->frame->prepare(buf->buf_cl, buf->width, buf->height, transform, static_cast<cl_mem*>(s->m->getInputBuf()));
 
-  if (prepare_only) {
-    return nullptr;
-  }
-
   s->m->addImage(net_input_buf, s->frame->buf_size);
 
   if (wbuf != nullptr) {
     auto net_extra_buf = s->wide_frame->prepare(wbuf->buf_cl, wbuf->width, wbuf->height, transform_wide, static_cast<cl_mem*>(s->m->getExtraBuf()));
     s->m->addExtra(net_extra_buf, s->wide_frame->buf_size);
   }
+
+  if (prepare_only) {
+    return nullptr;
+  }
+
   s->m->execute();
 
   return (ModelOutput*)&s->output;
