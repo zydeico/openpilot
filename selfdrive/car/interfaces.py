be1978e29c03d8843cf073cc9ddf84020fd2b378
Shane Smiskol <shane@smiskol.com>
2022-03-15 22:36:16 -0700

    stash

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -127,8 +127,6 @@ class CarInterfaceBase(ABC):
       events.add(EventName.wrongCarMode)
     if cs_out.espDisabled:
       events.add(EventName.espDisabled)
-    if cs_out.gasPressed and self.disengage_on_gas:
-      events.add(EventName.gasPressed)
     if cs_out.stockFcw:
       events.add(EventName.stockFcw)
     if cs_out.stockAeb:
--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -96,7 +96,8 @@ class Controls:
     self.CI, self.CP = get_car(self.can_sock, self.pm.sock['sendcan'])
 
     # see panda/board/safety_declarations.h for allowed values
-    self.CP.unsafeMode = 0 if Params().get_bool("DisengageOnGas") else 1
+    self.disengage_on_gas = Params().get_bool("DisengageOnGas")
+    self.CP.unsafeMode = 1 if not self.disengage_on_gas else 0
 
     # read params
     self.is_metric = params.get_bool("IsMetric")
